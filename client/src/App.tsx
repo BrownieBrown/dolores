7db3a1484b7106b385b3897cf5c4d4ec85730559
Marco Braun <marco.braun@exxeta.com>
2022-10-27 20:28:36 +0200

    Add scroll animations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,43 @@
 import React from 'react';
 import './App.css';
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+import { useRef } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const ref = useRef(null);
+
+    const options = {
+        smooth: true,
+    }
+
+    return (
+        <LocomotiveScrollProvider options={options} containerRef={ref}>
+            <main data-scroll-container ref={ref}>
+                <section className="intro" data-scroll-section>
+                    <h1>This is the Introduction section</h1>
+                </section>
+                <section className="contents" data-scroll-section>
+                    <h1
+                        data-scroll
+                        data-scroll-direction="horizontal"
+                        data-scroll-speed="9"
+                    >
+                        I Love React
+                    </h1>
+                    <h1
+                        data-scroll
+                        data-scroll-direction="vertical"
+                        data-scroll-speed="9" // Values provided here affect the animations
+                    >
+                        That's why I code every day
+                    </h1>
+                </section>
+                <section className="footer" data-scroll-section>
+                    <h1>Let's end the application with this Footer</h1>
+                </section>
+            </main>
+        </LocomotiveScrollProvider>
+    );
 }
 
 export default App;
