fa0b61c17f0547c82e0b1a0e34e21efec9f3c42d
Marco Braun <marco.braun@exxeta.com>
2022-11-05 14:56:24 +0100

    Create basic components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import styles from "./style";
+import { Navbar, Hero, Footer, Button, Stats } from "./components";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    <h1>Hello World!</h1>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Footer />
+      </div>
+    </div>
   </div>
 );
 
